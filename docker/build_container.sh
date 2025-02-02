845d9b63920178b3fd443da0e3d42850c401fa95
Leo Lee <songsub.leo.lee@gmail.com>
2025-02-02 15:47:06 +0900

    add docker container setup
    Signed-off-by: Leo Lee <songsub.leo.lee@gmail.com>

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $DOCKER_DIR
+
 DOCKER_UNAME=docker-$(id -un)
 DOCKER_NAME=dev-$(id -un)
 
-docker build . --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=$DOCKER_UNAME --build-arg --rm -t $DOCKER_NAME -f Dockerfile
+docker build . --rm \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --build-arg UNAME=$DOCKER_UNAME \
+    --tag $DOCKER_NAME \
+    --file Dockerfile
