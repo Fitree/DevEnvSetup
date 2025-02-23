ece7007d16fba75e5b7239a51c6836ccf63a82e6
Leo Lee <songsub.leo.lee@gmail.com>
2025-02-23 12:43:07 +0000

    Udate
    Signed-off-by: Leo Lee <songsub.leo.lee@gmail.com>

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -3,12 +3,12 @@
 DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DOCKER_DIR
 
-DOCKER_UNAME=docker-$(id -un)
-DOCKER_NAME=dev-ubt2204-cu124-py310
+# Source container configuration
+source ./container_config.sh
 
 docker build . --rm \
-    --build-arg UID=$(id -u) \
-    --build-arg GID=$(id -g) \
+    --build-arg UID=$DOCKER_UID \
+    --build-arg GID=$DOCKER_GID \
     --build-arg UNAME=$DOCKER_UNAME \
     --tag $DOCKER_NAME \
     --file Dockerfile
--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $DOCKER_DIR
+
+# Source container configuration
+source ./container_config.sh
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <workspace-dir> [container-name]"
     exit 1
@@ -7,13 +13,11 @@ fi
 
 WORKSPACE_DIR=$1
 if [ -z "$2" ]; then
-    echo "No container name provided. Using default: dev-ubt2204-cu124-py310"
-    CONTAINER_NAME="dev-ubt2204-cu124-py310"
+    echo "No container name provided. Using default: $DOCKER_NAME"
+    CONTAINER_NAME="$DOCKER_NAME"
 else
     CONTAINER_NAME=$2
 fi
-
-DOCKER_NAME=dev-ubt2204-cu124-py310
 
 SSH_DIR="$HOME/.ssh"
 if [ -d "$SSH_DIR" ]; then
@@ -24,12 +28,12 @@ else
 fi
 
 docker run -it \
-  --name $CONTAINER_NAME \
-  -e TERM=xterm-256color \
-  -u $(id -u):$(id -g) \
-  -v $WORKSPACE_DIR:/workspace \
-  --ipc=host \
-  --network host \
-  --shm-size=512g \
-  $MOUNT_SSH \
-  $DOCKER_NAME bash -c "python3 -c \"\$(curl -fsSL https://raw.githubusercontent.com/Fitree/dev-env-setup/refs/heads/main/scripts/autosetup.py)\" && zsh"
+    --name $CONTAINER_NAME \
+    -e TERM=xterm-256color \
+    -u $DOCKER_UID:$DOCKER_GID \
+    -v $WORKSPACE_DIR:/workspace \
+    --ipc=host \
+    --network host \
+    --shm-size=512g \
+    $MOUNT_SSH \
+    $DOCKER_NAME bash -c "python3 -c \"\$(curl -fsSL https://raw.githubusercontent.com/Fitree/dev-env-setup/refs/heads/main/scripts/autosetup.py)\" && zsh"
